5ad0e7bc0e82cc97f87cf7339a71e1b57f700de8
Parminder <parminderpalsingh9203@gmail.com>
2024-07-22 12:59:26 -0700

    Working on server search component

---
+++ components/servers/server-search.tsx
@@ -0,0 +1,114 @@
+"use client";
+import {
+  Hash,
+  Mic,
+  Search,
+  ShieldAlert,
+  ShieldCheck,
+  Video,
+} from "lucide-react";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { useEffect, useState } from "react";
+import { ChannelType, MemberRole } from "@prisma/client";
+
+interface SearchListData {
+  heading: string;
+  items: string[];
+  type: ChannelType;
+}
+
+interface ServerSearchProps {
+  data: SearchListData[];
+  members: {
+    name: string;
+    role: MemberRole;
+  }[];
+}
+
+const ServerSearch = ({ data, members }: ServerSearchProps) => {
+  const [open, setOpen] = useState(false);
+
+  const iconMap = {
+    [ChannelType.TEXT]: <Hash size={8} />,
+    [ChannelType.AUDIO]: <Mic size={8} />,
+    [ChannelType.VIDEO]: <Video size={8} />,
+  };
+
+  const roleIconMap = {
+    [MemberRole.GUEST]: "",
+    [MemberRole.MODERATOR]: <ShieldCheck />,
+    [MemberRole.ADMIN]: <ShieldAlert size={8}/>,
+  };
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+  return (
+    <>
+      <div className="border border-netural-200 p-1">
+        <button
+          className="flex items-center justify-between w-full"
+          onClick={() => setOpen(true)}
+        >
+          <div className="flex items-center">
+            <Search size={12} />
+            <p className="ml-2 text-zinc-300 text-xs">Search...</p>
+          </div>
+          <div className="text-xs bg-gray-800 px-1">
+            <kbd>
+              <span className="text-[9px]">⌘</span>+k
+            </kbd>
+          </div>
+        </button>
+      </div>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {data.map((item) => {
+            return (
+              <>
+                <CommandGroup heading={item.heading}>
+                  {item.items.map((channelName: string) => (
+                    <CommandItem>
+                      {iconMap[item.type]}
+                      {channelName}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+                <CommandSeparator />
+              </>
+            );
+          })}
+          <CommandGroup heading="Server Members">
+            {members.map((member) => (
+              <CommandItem>
+                {member.name}
+               {roleIconMap[member.role]}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default ServerSearch;
--- components/servers/server-sidebar.tsx
+++ components/servers/server-sidebar.tsx
@@ -2,6 +2,9 @@ import { db } from "@/lib/db";
 import ServerSidebarHeader from "./server-sidebar-header";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../ui/scroll-area";
+import ServerSearch from "./server-search";
+import { ChannelType } from "@prisma/client";
 
 const ServerSideBar = async ({ serverId }: { serverId: string }) => {
   const user = await initialProfile();
@@ -35,9 +38,48 @@ const ServerSideBar = async ({ serverId }: { serverId: string }) => {
   console.log("server::::", server);
   // console.log("role::::", role);
 
+  const textChannels = server.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+
+  console.log("textChannels:::", textChannels);
+
+  const members = server.members.map((member) => ({
+    name: member.profile.username,
+    role: member.role,
+  }));
+
   return (
     <div className="p-2">
       <ServerSidebarHeader server={server} role={role!} />
+      <ScrollArea className="mt-4">
+        <ServerSearch
+          data={[
+            {
+              heading: "Text Channels",
+              items: textChannels.map((channel) => channel.name),
+              type:ChannelType.TEXT
+            },
+            {
+              heading: "Audio Channels",
+              items: audioChannels.map((channel) => channel.name),
+              type:ChannelType.AUDIO
+            },
+            {
+              heading: "Video Channels",
+              items: videoChannels.map((channel) => channel.name),
+              type:ChannelType.VIDEO
+            },
+          ]}
+          members={members}
+        />
+      </ScrollArea>
     </div>
   );
 };
